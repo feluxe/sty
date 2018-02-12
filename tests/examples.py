5e43f32679ae66d90f93c3032c3741c6f4f3426d
feluxe <felix@meyerwolters.de>
2018-02-12 15:22:24 +0100

    Update README.md

--- tests/examples.py
+++ tests/examples.py
@@ -1,14 +1,12 @@
 from sty.register import FgRegister, BgRegister, RsRegister, EfRegister
 
-# Reset items
+
+print('\nGetting Started\n--------------------')
+
 ef = EfRegister()
 fg = FgRegister()
 bg = BgRegister()
 rs = RsRegister()
-
-# Getting Started
-
-print('\n')
 
 foo = fg.red + 'This is red text!' + fg.rs
 bar = bg.blue + 'This has a blue background!' + bg.rs
@@ -18,19 +16,30 @@ qui = fg(255, 10, 10) + 'This is red text using 24bit colors.' + fg.rs
 
 # Add new colors:
 
-
 fg.orange = ('rgb', (255, 150, 50))
 
 buf = fg.orange + 'Yay, Im orange.' + fg.rs
 
 print(foo, bar, baz, qux, qui, buf, sep='\n')
 
-# Hello World!
+
+print('\nHello World!\n--------------------')
+ef = EfRegister()
+fg = FgRegister()
+bg = BgRegister()
+rs = RsRegister()
+
 
 my_string = fg.black + bg.white + ef.bold + 'Hello world!' + rs.all
 print(my_string)
 
-# Italic:
+
+print('\nItalic\n--------------------')
+ef = EfRegister()
+fg = FgRegister()
+bg = BgRegister()
+rs = RsRegister()
+
 
 a = ef.italic + 'Italic.' + rs.italic
 
@@ -39,7 +48,12 @@ b = ef.i + fg.blue + 'Italic.' + rs.i + ' Not italic but blue.' + rs.fg
 
 print(a, b, sep='\n')
 
-# Bold:
+
+print('\nBold\n--------------------')
+ef = EfRegister()
+fg = FgRegister()
+bg = BgRegister()
+rs = RsRegister()
 
 a = ef.bold + 'Bold.' + rs.bold
 
@@ -48,7 +62,13 @@ b = ef.b + 'Bold.' + rs.b + fg.li_yellow + ' Not bold but yellow.' + rs.fg
 
 print(a, b, sep='\n')
 
-# Underlining:
+
+print('\nUnderlining\n--------------------')
+ef = EfRegister()
+fg = FgRegister()
+bg = BgRegister()
+rs = RsRegister()
+
 
 a = ef.underline + 'Underlined.' + rs.underline
 
@@ -57,7 +77,13 @@ b = ef.u + 'Underlined.' + rs.u + fg.green + ' Not underlined but green.' + rs.f
 
 print(a, b, sep='\n')
 
-# String coloring by name:
+
+print('\nString coloring by name\n--------------------')
+ef = EfRegister()
+fg = FgRegister()
+bg = BgRegister()
+rs = RsRegister()
+
 
 a = fg.blue + 'I have a blue foreground.' + rs.fg
 b = bg.li_cyan + 'I have a light cyan background' + rs.bg
@@ -65,7 +91,13 @@ c = fg.red + bg.green + 'I have a red fg and green bg.' + rs.all
 
 print(a, b, c, sep='\n')
 
-# String coloring by 8-bit number:
+
+print('\nString coloring by 8-bit number\n--------------------')
+ef = EfRegister()
+fg = FgRegister()
+bg = BgRegister()
+rs = RsRegister()
+
 
 a = fg(34) + 'I have a green foreground.' + rs.fg
 b = bg(133) + 'I have a pink background' + rs.bg
@@ -73,7 +105,13 @@ c = fg(226) + bg(19) + 'I have a light yellow fg and dark blue bg.' + rs.all
 
 print(a, b, c, sep='\n')
 
-# String coloring by 24-bit RGB value:
+
+print('\nString coloring by 24-bit RGB value\n--------------------')
+ef = EfRegister()
+fg = FgRegister()
+bg = BgRegister()
+rs = RsRegister()
+
 
 a = fg(10, 255, 10) + 'I have a green foreground.' + rs.fg
 b = bg(255, 150, 50) + 'I have an orange background' + rs.bg
@@ -81,50 +119,52 @@ c = fg(90, 90, 90) + bg(32, 32, 32) + 'Grey fg and dark grey bg.' + rs.all
 
 print(a, b, c, sep='\n')
 
-### Direct attribute customization
 
-
-ef.italic = ('sgr', 1)  # ef.italic now renders bold text.
-fg.red = ('sgr', 32)  # fg.red renders green text from now on.
-fg.blue = ('eightbit',
-           111)  # fg.blue renders blue text from now on (using an 8bit color code).
-fg.my_new_item = ('eightbit', 130)  # Create a new item that renders brown text.
-bg.green = ('rgb', (0, 128,
-                    255))  # bg.green renders blue text from now on (using a 24bit rgb code).
-rs.bold = ('sgr', 24)  # rs.all only resets the underline effect from now on.
-
-a = ef.italic + 'This is not italic any more' + rs.italic
-b = fg.red + 'This is not red any more.' + rs.fg
-c = fg.blue + 'This is not blue any more' + rs.fg
-d = fg.my_new_item + 'This is a new attribute' + rs.fg
-e = bg.green + 'This is not green bg any more.' + rs.bg
-f = 'This does not reset bold any more' + rs.bold + ' -> This is still bold' + rs.all
-
-print(a, b, c, d, e, f, sep='\n')
-
-# Reset items
+print('\nDirect attribute customization\n--------------------')
 ef = EfRegister()
 fg = FgRegister()
 bg = BgRegister()
 rs = RsRegister()
 
-### Dynamic attribute customization
+
+ef.italic = ('sgr', 4)  # ef.italic now renders underlined text.
+fg.red = ('sgr', 32)  # fg.red renders green text from now on.
+fg.blue = ('eightbit', 88)  # fg.blue renders red text from now on (using an 8bit color code).
+fg.my_new_item = ('eightbit', 130)  # Create a new item that renders brown text.
+bg.green = ('rgb', (0, 128,255))  # bg.green renders blue text from now on (using a 24bit rgb code).
+rs.bold = ('sgr', 24)  # rs.all only resets the underline effect from now on.
+
+a = ef.italic + 'This is not italic any more, but underlined' + rs.underline
+b = fg.red + 'This is not red any more, but green.' + rs.fg
+c = fg.blue + 'This is not blue any more, but red' + rs.fg
+d = fg.my_new_item + 'This is a new brown attribute' + rs.fg
+e = bg.green + 'This is not green bg any more, but blue.' + rs.bg
+f = ef.bold + 'This does not reset bold any more' + rs.bold \
+    + ' -> This is still bold' + rs.all
+
+print(a, b, c, d, e, f, sep='\n')
+
+print('\nDynamic attribute customization\n--------------------')
+ef = EfRegister()
+fg = FgRegister()
+bg = BgRegister()
+rs = RsRegister()
+
 
 my_color_name = 'special_teal'
 
 fg.set(my_color_name, 'eightbit', 51)
 
-a = fg.special_teal + 'This is teal text.' + fg.rs
+a = fg.special_teal + 'This is custom teal text.' + fg.rs
 
 print(a)
 
-# Reset items
+print('\nExtending the default registers\n--------------------')
 ef = EfRegister()
 fg = FgRegister()
 bg = BgRegister()
 rs = RsRegister()
 
-### Extending the default registers
 
 from sty.register import FgRegister
 
@@ -138,17 +178,16 @@ class MyFgRegister(FgRegister):
 
 fg = MyFgRegister()
 
-a = fg.orange + 'This is orange text.' + rs.fg
+a = fg.orange + 'This is custom orange text.' + rs.fg
 
 print(a)
 
-# Reset items
+print('\nReplace or add renderers\n--------------------')
 ef = EfRegister()
 fg = FgRegister()
 bg = BgRegister()
 rs = RsRegister()
 
-### Replace or add renderers
 
 from sty.register import FgRegister
 
@@ -173,7 +212,9 @@ fg = MyFgRegister()
 a = fg.orange + 'I have a orange background instead of an orange fg.' + rs.bg
 
 print(a)
-# Reset items
+
+
+print('\nChange call renderers\n--------------------')
 ef = EfRegister()
 fg = FgRegister()
 bg = BgRegister()
@@ -200,7 +241,7 @@ class MyFgRegister(FgRegister):
 
 fg = MyFgRegister()
 
-a = fg(90) + 'I have a colored background now.' + rs.bg
-b = fg(40, 50, 200) + 'I have a colored background now.' + rs.bg
+a = fg(90) + 'Colored bg instead of colored fg.' + rs.bg
+b = fg(40, 50, 200) + 'Colored bg instead of colored fg.' + rs.bg
 
 print(a, b, sep='\n')
